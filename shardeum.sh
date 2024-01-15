d86eb912fdfb22f885e53edfc3ea5b6035cf04c2
Mitesh Metha <41751429+Meetmetha@users.noreply.github.com>
2024-01-15 17:48:23 +0530

    Update shardeum.sh
    Fixed Directory Issue

--- shardeum.sh
+++ shardeum.sh
@@ -3,6 +3,8 @@ sudo apt install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
-cd .shardeum
-sudo ./shell.sh
+sudo su
+shardeum_directory=$(find ~ -type d -name ".shardeum") 
+cd "$shardeum_directory"
+sudo bash shell.sh
 operator-cli gui start
