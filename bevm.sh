2b94bd74cecd413393fdc2e12573dd4b2b90fdb1
Mitesh Metha <41751429+Meetmetha@users.noreply.github.com>
2024-01-24 19:50:30 +0530

    Update bevm.sh

--- bevm.sh
+++ bevm.sh
@@ -9,7 +9,7 @@ sudo apt install docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 cd /var/lib
-mkdir bevmnode
+sudo mkdir bevmnode
 sudo docker pull btclayer2/bevm:v0.1.1
 sudo chmod 777 /var/run/docker.sock
 sudo docker run -d -v /var/lib/bevmnode:/root/.local/share/bevm btclayer2/bevm:v0.1.1 bevm --chain=testnet --name="$name_parameter" --pruning=archive --telemetry-url="wss://telemetry.bevm.io/submit 0”
